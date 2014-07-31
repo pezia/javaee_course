8e4c9023707d6921fcf02365ed1ce1a7fc92a388
Zsolt Petrik <pezia@pezia.hu>
2014-07-31 23:12:19 +0200

    EAR deployment descriptor cleanup, Hibernate JPA provider classname fix

--- QualysoftWebshop-ejb/src/java/eu/md/qualysoft/webshop/repository/ProductRepository.java
+++ QualysoftWebshop-ejb/src/java/eu/md/qualysoft/webshop/repository/ProductRepository.java
@@ -5,17 +5,20 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+/**
+ * Product repository for handling Product entities
+ * 
+ * @author Zsolt Petrik
+ */
 @Stateless
 public class ProductRepository implements ProductRepositoryLocal {
-    
+
     @PersistenceContext
     EntityManager em;
-    
+
     @Override
     public Product findById(long id) {
-        
         return null;
-        
     }
 
 }
